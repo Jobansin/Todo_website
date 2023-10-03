2f3e8e0b25ccfac13417175dc157eab7710f73ca
Jobansin <jobanpreetsngh10@gmail.com>
2023-10-02 23:43:17 -0400

    Able to run

--- index.tsx
+++ index.tsx
@@ -2,4 +2,8 @@ Bun.serve({
     hostname: "localhost",
     port: 3000,
     fetch: fetchHandler,
-  });
+});
+
+function fetchHandler(request: Request): Response{
+    return new Response(Bun.file("index.html"));
+}
